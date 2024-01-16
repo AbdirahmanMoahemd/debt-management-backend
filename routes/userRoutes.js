d3dce1af4cf780394009f8f0acebbc46d77ac5f8
AbdirahmanMoahemd <kaah6978@gmail.com>
2024-01-16 11:56:27 +0300

    test

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,10 @@ import {
 } from "../controllers/usersController.js";
 import { admin, protect } from "../middlewares/authMiddleware.js";
 
-router.route("/").post(registerUser).get(protect, admin, getUsers);
+router
+  .route("/")
+  .post(protect, admin, registerUser)
+  .get(protect, admin, getUsers);
 router.route("/login").post(login);
 router.route("/profile/:id").post(protect, getUserProfileById);
 router.route("/profile/update/:id").post(protect, updateProfile);
